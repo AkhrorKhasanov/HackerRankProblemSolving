fc0040b855f0c6a698f7fc190ed3b5c371e7ad0f
AkhrorKhasanov <ahror.hasanov.97@mail.ru>
2025-06-03 13:46:19 +0500

    03.06.2025 13:46

---
+++ Implementation/Utopian Tree.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'utopianTree' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def utopianTree(n):
+    a = 1
+    c = 1
+    if n == 0:
+        return a
+    while c <= n:
+        if c % 2 == 1:
+            a *= 2
+        else:
+            a += 1
+        c += 1
+    return a
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = utopianTree(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
